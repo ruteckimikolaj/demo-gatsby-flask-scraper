5f0e82ddb513353c5c715c2dfe666d82bfd00c89
Miko <ruteckimikolaj@gmail.com>
2020-02-22 16:27:06 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,73 @@
+require(`dotenv`).config({
+  path: `.env`,
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `NBA scraper`,
+    description: `NBA Scraper is here`,
+    siteTitleAlt: `NBA scraper`,
+    author: `Mikolaj Rutecki`,
+        // Used for the title template on pages other than the index site
+    siteTitle: `NBA scraper`,
+    // Default title of the page
+    // Can be used for e.g. JSONLD
+    siteHeadline: `NBA scraper for interview`,
+    // Used for SEO
+    siteDescription: `NBA Scraper for in`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Cara - @lekoarts/gatsby-theme-cara`,
+        short_name: `Cara`,
+        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ],
+};
---
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -0,0 +1,51 @@
+/** @jsx jsx */
+import { Footer as ThemeFooter, Styled, Flex, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <ThemeFooter>
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      {/*Copyright &copy; {new Date().getFullYear()}. All rights reserved.*/}
+      <br />
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+        {` `}
+        <Styled.a
+          aria-label="Link to the theme's GitHub repository"
+          sx={{ ml: 2 }}
+          href="https://nba.com"
+        >
+          Created
+        </Styled.a>
+        <div sx={{ mx: 1 }}>by</div>
+        {` `}
+        <Styled.a aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en">
+          Mikolaj Rutecki
+        </Styled.a>
+      </Flex>
+    </ThemeFooter>
+  )
+}
+
+export default Footer
